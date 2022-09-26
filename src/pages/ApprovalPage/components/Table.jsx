7a635e8ee1a2042849392199c048c3d28ad163db
Serj-no-comments <76804083+Serj-no-comments@users.noreply.github.com>
2022-09-26 19:58:41 +0400

    Table
    Добавил массив из 3х объектов, который включает согласующего, его решение и комментарий, если он нужен. Также сделал функцию if/else для покраса решения в нужный цвет

--- src/pages/ApprovalPage/components/Table.jsx
+++ src/pages/ApprovalPage/components/Table.jsx
@@ -9,31 +9,48 @@ import {
     Paper
 } from '@mui/material'
 
-const style={
-    topRow:{
-        backgroundColor: '#e0e0e0'
+const style = {
+    topRow: {
+        backgroundColor: '#e0e0e0',
+        fontSize: 20
+    },
+    disagree: {
+        color: '#ff3d00'
+    },
+    agree: {
+        color: '#4caf50'
     }
+
 }
 
-export const ApprovalTable=()=>{
+const agreeDisAgree = (prop) => {
+    if (prop === 'отклонено') {
+        return style.disagree
+    } else {
+        return style.agree
+    }
+
+}
+
+export const ApprovalTable = () => {
     function createData(name, status, comment) {
         return { name, status, comment }
     }
 
     const rows = [
-        createData('Петров Виктор Фёдорович', 'отклонено', 'Комментарий'),
+        createData('Петров Виктор Фёдорович', 'отклонено', 'Пункт 3.1: Изменить количество закупаемого товара с 5 до 10'),
         createData('Колмагорская Екатерина Викторовна', 'согласовано', ''),
         createData('Евсюков Дмитрий Анатольевич', 'согласовано', ''),
     ]
 
-    return(
+    return (
         <TableContainer component={Paper}>
             <Table aria-label='simple table'>
                 <TableHead>
                     <TableRow>
-                        <TableCell align='center' width={700} style={style.topRow}>Согласующий</TableCell>
-                        <TableCell align='center' style={style.topRow}>Решение</TableCell>
-                        <TableCell align='center' style={style.topRow}>Комментарий</TableCell>
+                        <TableCell align='center' width={700} style={style.topRow} >Согласующий</TableCell>
+                        <TableCell align='center' width={700} style={style.topRow} >Решение</TableCell>
+                        <TableCell align='center' width={700} style={style.topRow} >Комментарий</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
@@ -41,7 +58,7 @@ export const ApprovalTable=()=>{
                         // eslint-disable-next-line react/jsx-key
                         <TableRow>
                             <TableCell align='center'>{row.name}</TableCell>
-                            <TableCell align='center'>{row.status}</TableCell>
+                            <TableCell align='center' style={agreeDisAgree(row.status)}>{row.status}</TableCell>
                             <TableCell align='center'>{row.comment}</TableCell>
                         </TableRow>
                     ))}

f511aa76dc102fa10d8249a9cfa856eee4722837
Serj-no-comments <76804083+Serj-no-comments@users.noreply.github.com>
2022-09-26 16:39:37 +0400

    ApprovalPage
    Начал верстку страницы.

--- src/pages/ApprovalPage/components/ApprovalPage.jsx
+++ src/pages/ApprovalPage/components/ApprovalPage.jsx
@@ -1,7 +1,25 @@
-import { React, useState } from 'react';
-import './ApprovalPage.css'
-import { DocsAndEnd } from './docsAndEnd';
 
+import React from 'react'
+import { Stack,
+    Box,
+    Typography,
+    Button,
+    Paper
+} from '@mui/material'
+import ArticleIcon from '@mui/icons-material/Article';
+import { ApprovalTable } from './Table'
+
+const styles ={
+    button:{
+        margin: 5
+    },
+    table:{
+        margin: 20
+    },
+    padding:{
+        padding:20
+    }
+};
 
 
 export const ApprovalPage = () => {
@@ -9,86 +27,75 @@ export const ApprovalPage = () => {
     // const handleOpen = () => setOpen(true);
     // const handleClose = () => setOpen(false);
     return (
-        <div>
-            <div className="mainBlock">
-                <div className="">
-                    <div className="projectName">
-                        <p className="nameBlock">
-                            Согласование проекта:
-                            <p className='appName'>Контракт по закупке канцелярский товаров</p>
-                        </p>
-                        <DocsAndEnd />
-                    </div>
-                    <hr className='line'></hr>
-                    <div className="informHead">
-                        <div className="approvalInf">
-                            <p className="textbord">
-                                <p className="headText">Управляющий проектом:</p>
-                                <span>Иванов Иван Иванович</span>
-                            </p>
-
-                            <p className="textbord">
-                                <p className="headText">Статус:</p>
-                                отклонено
-                            </p>
-                        </div>
-
-
-
-                    </div>
-                    <div className="tablebox">
-                        <div className="listName">
-                            <p>
-                                Лист согласования
-                            </p>
-                        </div>
-                        <table class="iksweb">
-                            <tbody>
-                                <tr className="tableHead">
-                                    <td className='columnName'>Согласующий</td>
-                                    <td className='columnName'>Решение</td>
-                                    <td className='columnName'>Комментарий</td>
-
-                                </tr>
-                                <tr>
-                                    <td>Петров Виктор Фёдорович</td>
-                                    <td>
-                                        <p className="disagree">
-                                            Отклонено:
-                                        </p>
-                                        __.__.20__ , __.__.__
-                                    </td>
-                                    <td>ГК-2019-1:
-                                        <p>П-5.3 - поменять количество с 4 на 7</p>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Колмагорская Инна Витальевна</td>
-                                    <td>
-                                        <p className="agree">
-                                            Согласовано:
-                                        </p>
-                                        __.__.20__ , __.__.__
-                                    </td>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <td>Федорчук Петр Сергеевич</td>
-                                    <td><p className="agree">
-                                        Согласовано:
-                                    </p>
-                                        __.__.20__ , __.__.__</td>
-                                    <td></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-
-        </div >
-
+        <Stack spacing={2} flex={1} style={styles.table} >
+            <Typography variant='h2' align='center'>
+                Согласование документов
+            </Typography>
+            <Stack direction="row"
+                justifyContent="space-around"
+                alignItems="center"
+                spacing={2}>
+                <Box >
+                    <Typography variant='h4' >
+                        Согласование проекта:
+                    </Typography>
+                    <Typography variant='subtitle1' gutterBottom>
+                        Контракт по закупке канцелярских товаров
+                    </Typography>
+                    <Button variant='outlined' startIcon={<ArticleIcon />}>Документы</Button>
+                </Box>
+                <Box
+                    sx={{
+                        width: 300,
+                        height: 350,
+                    }}
+                    component={Paper}
+                    style={styles.padding}
+                >
+                    <Typography variant='h5' align='center'>
+                        Информация о проекте
+                    </Typography>
+                    <Stack direction="row">
+                        <Typography variant='h7' >
+                            Управляющий проектом:
+                        </Typography>
+                        <Typography gutterBottom>
+                            Иванов Иван Иванович
+                        </Typography>
+                    </Stack>
+                    <Stack direction="row">
+                        <Typography variant='h7' >
+                            Статус проекта:
+                        </Typography>
+                        <Typography gutterBottom>
+                            отклонен
+                        </Typography>
+                    </Stack>
+                    <Stack direction="row">
+                        <Typography variant='h7' >
+                            Решение:
+                        </Typography>
+                        <Typography gutterBottom>
+                            отклонен
+                        </Typography>
+                    </Stack>
+                    <Stack>
+                        <Button variant='outlined' style={styles.button}>Изменение решения</Button>
+                        <Button variant='outlined' >Изменение статуса проекта</Button>
+                    </Stack>
+                </Box>
+            </Stack>
+            <Box>
+                <Typography variant='h5' align='center'>
+                    Лист Согласования
+                </Typography>
+                <ApprovalTable />
+            </Box>
+            <Stack direction="row" justifyContent="end">
+                <Button variant='outlined'
+                    sx={{ width: 300 }}
+                    style={styles.answerButton}>Ответить на комментарий</Button>
+            </Stack>
+        </Stack>
     )
 }
-
-

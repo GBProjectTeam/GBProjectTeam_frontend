f6987b1c0ac7727b4290abef36800990d3b1457c
Serj-no-comments <76804083+Serj-no-comments@users.noreply.github.com>
2022-09-27 21:51:27 +0400

    ProjectInfoCard
    Переименование документа и внесение изменений

--- src/pages/ApprovalPage/components/ProjectInfoCard.jsx
+++ src/pages/ApprovalPage/components/ProjectInfoCard.jsx
@@ -9,42 +9,50 @@ import {
 } from '@mui/material'
 import { mainUser } from '../constants/mainUser'
 
-export const ProjectInformCard = () => {
+export const ProjectInfoCard = () => {
     return (
         <Card >
             <CardHeader title='Информация о проекте' />
             <CardContent>
+
                 <Typography >
                     Автор проекта: {mainUser.name}
                 </Typography>
+
                 <Typography >
                     Статус проекта: {mainUser.prodStatus}
                 </Typography>
+
                 <Typography >
                     Решение: {mainUser.decision}
                 </Typography>
+
             </CardContent>
-            <CardContent sx={{
-                minWidth: 275,
-                padding: '15px',
-                display: 'flex',
-                alignItems: 'center',
-                flexDirection: 'column'
-            }}
+
+            <CardContent
+                sx={{
+                    minWidth: 275,
+                    padding: '15px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    flexDirection: 'column'
+                }}
             >
                 <CardActions >
                     <Button sx={{ borderRadius: 20, minWidth: 250 }}
                         size='small'
                         variant='outlined'
                     >
-                        Изменить решение</Button>
+                        Изменить решение
+                    </Button>
                 </CardActions>
                 <CardActions >
                     <Button sx={{ borderRadius: 20, minWidth: 250 }}
                         size='small'
                         variant='outlined'
                     >
-                        Изменить статус проекта</Button>
+                        Изменить статус проекта
+                    </Button>
                 </CardActions>
             </CardContent>
         </Card>

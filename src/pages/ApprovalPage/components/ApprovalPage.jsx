4627ddc0279dbcee973cec33312262c53c1bae95
Serj-no-comments <76804083+Serj-no-comments@users.noreply.github.com>
2022-09-28 16:54:43 +0400

    ApprovalPage
    Внес изменения касаемо дизайна страницы и кода

--- src/pages/ApprovalPage/components/ApprovalPage.jsx
+++ src/pages/ApprovalPage/components/ApprovalPage.jsx
@@ -1,33 +1,43 @@
 import React from 'react'
 import {
     Stack,
-    Box,
     Typography,
 } from '@mui/material'
 import { Documents } from './Documents'
+import { ProjectInfoCard } from './ProjectInfoCard'
 import { ApprovalTable } from './ApprovalTable'
-import { ProjectInformCard } from './ProjectInfoCard'
 
 export const ApprovalPage = () => {
     return (
+        
         <Stack
             spacing={2}
             flex={1}
-            justifyContent='space-beetwen'
+            justifyContent='space-between'
         >
-            <Typography variant='h2' align='center' sx={{ padding: '20px' }}>
+            <Typography
+                variant='h2'
+                align='center'
+                sx={{ padding: '20px' }}
+            >
                 Согласование документов
             </Typography>
-            <Stack direction='row'
+            
+            <Stack
+                direction='row'
                 justifyContent='space-around'
                 alignItems='center'
-                spacing={2}>
-                <Box>
-                    <Documents />
-                </Box>
-                <ProjectInformCard />
+                spacing={2}
+            >
+                
+                <Documents />
+                
+                <ProjectInfoCard />
+            
             </Stack>
+            
             <ApprovalTable />
+        
         </Stack>
     )
 }

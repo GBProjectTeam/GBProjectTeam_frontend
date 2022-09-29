d2e493c373742903bf6001bd305370a7fb183612
kirill3518 <kirill3518@mail.ru>
2022-09-29 20:28:34 +0500

    Changed commponents of header

--- src/common/Header/components/Header.jsx
+++ src/common/Header/components/Header.jsx
@@ -1,187 +1,130 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import AppBar from '@mui/material/AppBar'
-import Box from '@mui/material/Box'
-import Toolbar from '@mui/material/Toolbar'
-// import IconButton from '@mui/material/IconButton'
-import Menu from '@mui/material/Menu'
-// import MenuIcon from '@mui/icons-material/Menu'
-import IconButton from '@mui/material/IconButton'
-import Tooltip from '@mui/material/Tooltip'
-import MenuItem from '@mui/material/MenuItem'
-import Avatar from '@mui/material/Avatar'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
-import Container from '@mui/material/Container'
-import AddIcon from '@mui/icons-material/Add'
-import SearchIcon from '@mui/icons-material/Search'
-import { styled, alpha } from '@mui/material/styles'
-import InputBase from '@mui/material/InputBase'
-
-// const pages = ['ПРОЕКТЫ', 'АРХИВ', 'СОЗДАТЬ НОВЫЙ']
-// const settings = ['Личный кабинет', 'Выйти']
+import {
+    Box,
+    Menu,
+    MenuItem,
+    Typography,
+    Button,
+    TextField,
+    InputAdornment
+} from '@mui/material'
+import {
+    Add,
+    Search,
+    AccountCircle
+} from '@mui/icons-material'
 
 export const Header = () => {
+
     const navigate = useNavigate()
 
-    // const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null)
+    const [anchorEl, setAnchorEl] = React.useState(null)
 
-    // const handleOpenNavMenu = (event) => {
-    //     setAnchorElNav(event.currentTarget);
-    // };
+    const open = Boolean(anchorEl)
 
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget)
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget)
     }
 
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null)
+    const handleClose = () => {
+        setAnchorEl(null)
     }
-
-    const Search = styled('div')(({ theme }) => ({
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: alpha(theme.palette.common.white, 0.15),
-        '&:hover': {
-            backgroundColor: alpha(theme.palette.common.white, 0.25),
-        },
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing(1),
-            width: 'auto',
-        },
-    }))
-
-    const SearchIconWrapper = styled('div')(({ theme }) => ({
-        padding: theme.spacing(0, 2),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    }))
-
-    const StyledInputBase = styled(InputBase)(({ theme }) => ({
-        color: 'inherit',
-        '& .MuiInputBase-input': {
-            padding: theme.spacing(1, 1, 1, 0),
-            // vertical padding + font size from searchIcon
-            paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-            transition: theme.transitions.create('width'),
-            width: '100%',
-            [theme.breakpoints.up('sm')]: {
-                width: '12ch',
-                '&:focus': {
-                    width: '20ch',
-                },
-            },
-        },
-    }))
 
     return (
 
         <Box
-            component='main'
             sx={{
-                mx: 2,
                 display: 'flex',
-                flexGrow: 1,
-                justifyContent: 'center',
+                alignItems: 'flex-start'
             }}
         >
-            <AppBar position='static'>
-                <Container maxWidth='xl'>
-                    <Toolbar disableGutters>
-                        <Typography
-                            variant='h6'
-                            noWrap
-                            component='a'
-                            href='/'
-                            sx={{
-                                mr: 2,
-                                display: { xs: 'none', md: 'flex' },
-                                fontFamily: 'monospace',
-                                fontWeight: 700,
-                                letterSpacing: '.3rem',
-                                color: 'inherit',
-                                textDecoration: 'none',
-                            }}
-                        >
-                            DocsApproval
-                        </Typography>
-                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                            <Button
-                                key={'projects'}
-                                onClick={() => navigate('/projects')}
-                                sx={{ my: 2, color: 'white', display: 'block' }}
-                                variant='outlined'
-                            >
-                                {'ПРОЕКТЫ'}
-                            </Button>
-                            <Search
-                                sx={{ flexGrow: 0.4, my: 2 }}
-                            >
-                                <SearchIconWrapper>
-                                    <SearchIcon />
-                                </SearchIconWrapper>
-                                <StyledInputBase
-                                    placeholder='Поиск по проектам'
-                                    inputProps={{ 'aria-label': 'search' }}
-                                />
-                            </Search>
-                            <Button
-                                key={'archive'}
-                                onClick={() => navigate('/archive')}
-                                sx={{ my: 2, color: 'white', display: 'block' }}
-                            >
-                                {'АРХИВ'}
-                            </Button>
-                            <Button
-                                key={'createProject'}
-                                onClick={() => navigate('/createProject')}
-                                sx={{ my: 2, color: 'white', display: 'flex' }}
-                                startIcon={<AddIcon />}
-                            >
-                                {'СОЗДАТЬ НОВЫЙ'}
-                            </Button>
-                        </Box>
-                        <Box sx={{ flexGrow: 0 }}>
-                            <Tooltip title='Open settings'>
-                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                    <Avatar alt='Remy Sharp' src='' />
-                                    <p>Василий Пупкин</p>
-                                </IconButton>
-                            </Tooltip>
-                            <Menu
-                                sx={{ mt: '45px' }}
-                                id='menu-appbar'
-                                anchorEl={anchorElUser}
-                                anchorOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                open={Boolean(anchorElUser)}
-                                onClose={handleCloseUserMenu}
-                            >
-                                <MenuItem key={'personalArea'} onClick={() => navigate('/personalArea')}>
-                                    <Typography textAlign='center'>{'Личный кабинет'}</Typography>
-                                </MenuItem>
-                                <MenuItem key={'exit'} onClick={() => navigate('/exit')}>
-                                    <Typography textAlign='center'>{'Выйти'}</Typography>
-                                </MenuItem>
-                            </Menu>
-                        </Box>
-                    </Toolbar>
-                </Container>
-            </AppBar>
+
+            <Typography
+                variant='h4'
+                sx={{
+                    margin: 1
+                }}
+            >
+                DocsApproval
+            </Typography>
+
+            <Button
+                variant='outlined'
+                onClick={() => navigate('/projects')}
+                sx={{
+                    borderRadius: 10,
+                    margin: 1
+                }}
+            >
+                Проекты
+            </Button>
+
+            <TextField
+                id='outlined-basic'
+                placeholder='Поиск по проектам'
+                size='small'
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position='start'>
+                            <Search />
+                        </InputAdornment>
+                    ),
+                }}
+                variant='outlined'
+                sx={{
+                    // borderRadius: 20,
+                    margin: 1,
+                }}
+            />
+
+            <Button
+                variant='outlined'
+                onClick={() => navigate('/archive')}
+                sx={{
+                    borderRadius: 20,
+                    margin: 1
+                }}
+            >
+                Архив
+            </Button>
+
+            <Button
+                variant='contained'
+                onClick={() => navigate('/createProject')}
+                startIcon={<Add />}
+                sx={{
+                    margin: 1,
+                    borderRadius: 20,
+
+                }}
+            >
+                Создать новый
+            </Button>
+
+            <Button
+                variant='outlined'
+                startIcon={<AccountCircle />}
+                onClick={handleClick}
+                sx={{
+                    margin: 1,
+                    marginLeft: 'auto',
+                    borderRadius: 20,
+
+                }}
+            >
+                Василий Пупкин
+            </Button>
+
+            <Menu
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+            >
+                <MenuItem onClick={() => navigate('/personalArea')}>Личный кабинет</MenuItem>
+                <MenuItem onClick={() => navigate('/exit')}>Выйти</MenuItem>
+            </Menu>
+
         </Box>
     )
 }

b9fdfc9b4bb35d04acabbcbcfd64329dbc818553
SchooKish <66248485+SchooKish@users.noreply.github.com>
2022-10-03 23:20:14 +0300

    import MainPage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import { ApprovalPage } from './ApprovalPage/components/ApprovalPage.jsx'
 import { LoginPage } from './LoginPage/components/LoginPage.jsx'
+import { MainPage } from './MainPage/components/MainPage.jsx'
 
 export {
+    MainPage,
     ApprovalPage,
-    LoginPage
+    LoginPage,
 }

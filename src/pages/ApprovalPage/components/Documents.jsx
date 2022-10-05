49629365391609a358261fa2729bc967d1846cf9
Serj-no-comments <76804083+Serj-no-comments@users.noreply.github.com>
2022-10-05 18:08:26 +0400

    Documents
    Добавление Modal и его настройка. При открытии в окне ничего не отображается

--- src/pages/ApprovalPage/components/Documents.jsx
+++ src/pages/ApprovalPage/components/Documents.jsx
@@ -1,33 +1,18 @@
 import React from 'react'
 import {
-    Button,
     Typography,
-    Modal,
-    Link,
     Stack
 } from '@mui/material'
 import { Article } from '@mui/icons-material'
-import { ButtonModal } from '../../../common/Modal/components/ButtonModal'
-
-const stackStyle = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4
-}
+import { Modal } from '../../../common/Modal/components/Modal'
 
 export const Documents = () => {
     const [open, setOpen] = React.useState(false)
-    const handleOpen = () => setOpen(true)
-    const handleClose = () => setOpen(false)
 
     return (
         <>
             <Stack spacing={2}>
+
                 <Typography variant='h4'>
                     Согласование проекта:
                 </Typography>
@@ -36,31 +21,17 @@ export const Documents = () => {
                     Контракт по закупке канцелярских товаров
                 </Typography>
 
-                <Button
-                    sx={{
-                        borderRadius: 20,
-                        align: 'center',
-                        maxWidth: 200,
-                    }}
-                    onClick={handleOpen}
-                    variant='contained'
-                    startIcon={<Article />}
-                >
-                    Документы
-                </Button>
+                <Modal
+                    button='label'
+                    isOpen={open}
+                    onOpen={() => setOpen(true)}
+                    onClose={() => setOpen(false)}
+                    icon={<Article />}
+                    label='Документы'
+
+                />
             </Stack>
 
-            <Modal
-                open={open}
-                onClose={handleClose}
-            >
-                <Stack sx={stackStyle} >
-                    <Link href='#'>ГК-2018-1</Link>
-                    <Link href='#'>ДГ-2019-3</Link>
-                </Stack>
-            </Modal>
-
-            <ButtonModal button='edit' />
         </>
     )
 }

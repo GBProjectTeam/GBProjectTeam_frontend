be254b7d765cceb03a9aaa4083597c2bfffd76a2
Serj-no-comments <76804083+Serj-no-comments@users.noreply.github.com>
2022-10-08 19:12:53 +0400

    Documents
    Изменение стилей и удаление элементов верстки

--- src/pages/ApprovalPage/components/Documents.jsx
+++ src/pages/ApprovalPage/components/Documents.jsx
@@ -7,41 +7,33 @@ import {
 import { Article } from '@mui/icons-material'
 import { Modal } from '../../../common/Modal/components/Modal'
 
-const stackStyle = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-}
-
 export const Documents = () => {
     const [open, setOpen] = React.useState(false)
 
     return (
-        <>
-            <Stack spacing={2}>
-                <Typography variant='h4'>
-                    Согласование проекта:
-                </Typography>
+        <Stack spacing={2}>
+            <Typography variant='h4'>
+                Согласование проекта:
+            </Typography>
 
-                <Typography variant='h3' fontWeight='fontWeightBold'>
-                    Контракт по закупке канцелярских товаров
-                </Typography>
+            <Typography variant='h3' fontWeight='fontWeightBold'>
+                Контракт по закупке канцелярских товаров
+            </Typography>
 
-                <Modal
-                    button='label'
-                    isOpen={open}
-                    onOpen={() => setOpen(true)}
-                    onClose={() => setOpen(false)}
-                    icon={<Article />}
-                    label='Документы'
-                >
-                    <Stack sx={stackStyle} >
-                        <Link href='#'>ГК-2018-1</Link>
-                        <Link href='#'>ДГ-2019-3</Link>
-                    </Stack>
-                </Modal >
-            </Stack>
-        </>
+            <Modal
+                button='label'
+                isOpen={open}
+                onOpen={() => setOpen(true)}
+                onClose={() => setOpen(false)}
+                icon={<Article />}
+                label='Документы'
+                title='Документы проекта'
+            >
+                <Stack>
+                    <Link href='#'>ГК-2018-1</Link>
+                    <Link href='#'>ДГ-2019-3</Link>
+                </Stack>
+            </Modal>
+        </Stack>
     )
 }

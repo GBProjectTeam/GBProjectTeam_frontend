fba961a96598da9a270cc523c45fde4e2e7a941c
kirill3518 <kirill3518@mail.ru>
2022-10-10 13:22:23 +0500

    Changed ProjectsPage

--- src/pages/ProjectsPage/components/ProjectsPage.jsx
+++ src/pages/ProjectsPage/components/ProjectsPage.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import {
+    Stack,
+    Typography,
+} from '@mui/material'
+import {
+    DataGrid
+} from '@mui/x-data-grid'
+import { grey } from '@mui/material/colors'
+import { rows } from '../constants/rows'
+import { columns } from '../constants/columns'
 
 export const ProjectsPage = () => {
+    const navigate = useNavigate()
     return (
-        <div>
-            Страница проектов
-        </div>
+        <Stack
+            flexDirection='column'
+            direction='flex'
+            flex={1}
+            sx={{
+                '& .header-class-name': {
+                    fontWeight: 'bold',
+                    backgroundColor: grey[300],
+                    border: 1,
+                    fontSize: '150%',
+                },
+                '& .cell-class-name': {
+                    border: 1,
+                },
+            }}
+        >
+            <Typography
+                variant='h2'
+                align='center'
+                sx={{ padding: '20px' }}
+            >
+                Проекты
+            </Typography>
+
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                hideFooter={true}
+                sx={{
+                    borderTop: 1
+                }}
+                onRowClick={(params) => navigate(`/approval/${params.id}`)}
+            />
+        </Stack>
     )
 }

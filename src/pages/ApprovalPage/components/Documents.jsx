0311c58e6e81dc2256556c8102c02a7e89d19cbc
Serj-no-comments <76804083+Serj-no-comments@users.noreply.github.com>
2022-10-11 12:16:17 +0400

    Documents
    Добавление кнопки редактирования согласования проекта

--- src/pages/ApprovalPage/components/Documents.jsx
+++ src/pages/ApprovalPage/components/Documents.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import {
     Typography,
     Stack,
-    Link
+    Link,
+    IconButton
 } from '@mui/material'
 import { Article } from '@mui/icons-material'
+import CreateIcon from '@mui/icons-material/Create'
 import { Modal } from '../../../common/Modal/components/Modal'
 
 export const Documents = () => {
@@ -20,20 +22,33 @@ export const Documents = () => {
                 Контракт по закупке канцелярских товаров
             </Typography>
 
-            <Modal
-                button='label'
-                isOpen={open}
-                onOpen={() => setOpen(true)}
-                onClose={() => setOpen(false)}
-                icon={<Article />}
-                label='Документы'
-                title='Документы проекта'
-            >
-                <Stack>
-                    <Link href='#'>ГК-2018-1</Link>
-                    <Link href='#'>ДГ-2019-3</Link>
-                </Stack>
-            </Modal>
+            <Stack direction='row' spacing={2}>
+                <Modal
+                    button='label'
+                    isOpen={open}
+                    onOpen={() => setOpen(true)}
+                    onClose={() => setOpen(false)}
+                    icon={<Article />}
+                    label='Документы'
+                    title='Документы проекта'
+                >
+                    <Stack>
+                        <Link href='#'>ГК-2018-1</Link>
+                        <Link href='#'>ДГ-2019-3</Link>
+                    </Stack>
+                </Modal>
+                
+                <IconButton
+                    variant='contained'
+                    sx={{
+                        borderRadius: '5px',
+                        align: 'center',
+                        maxWidth: 40,
+                    }}
+                >
+                    <CreateIcon />
+                </IconButton>
+            </Stack>
         </Stack>
     )
 }

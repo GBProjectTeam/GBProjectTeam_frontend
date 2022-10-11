cb4a89c320b4f743fe4b23813efacc092da3a3c8
Serj-no-comments <76804083+Serj-no-comments@users.noreply.github.com>
2022-10-11 17:27:22 +0400

    Documents
    Внес изменения в кнопку

--- src/pages/ApprovalPage/components/Documents.jsx
+++ src/pages/ApprovalPage/components/Documents.jsx
@@ -6,7 +6,7 @@ import {
     Button
 } from '@mui/material'
 import { Article } from '@mui/icons-material'
-import CreateIcon from '@mui/icons-material/Create'
+import { Create } from '@mui/icons-material'
 import { Modal } from '../../../common/Modal/components/Modal'
 
 export const Documents = () => {
@@ -37,13 +37,14 @@ export const Documents = () => {
                         <Link href='#'>ДГ-2019-3</Link>
                     </Stack>
                 </Modal>
-                
+
                 <Button
                     variant='outlined'
-                    startIcon={<CreateIcon />}
-                    sx={{ borderRadius: '20px',
+                    startIcon={<Create />}
+                    sx={{
+                        borderRadius: '20px',
                         align: 'center',
-                        maxWidth: 400,
+                        width: 'fit-content',
                     }}
                 >
                     Редактировать проект

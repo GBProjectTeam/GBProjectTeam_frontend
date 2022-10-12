58ff778059c759360de5056dadfa966d52b1a30b
Serj-no-comments <76804083+Serj-no-comments@users.noreply.github.com>
2022-10-12 16:44:09 +0400

    PersonalArea
    Страница Личного кабинета

--- src/pages/PersonalArea/components/PersonalArea.jsx
+++ src/pages/PersonalArea/components/PersonalArea.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import {
+    Stack,
+    Typography
+} from '@mui/material'
+import { PersonlAvatar } from './PersonlAvatar'
+import { Mail } from './Mail'
 
 export const PersonalArea = () => {
     return (
-        <div>
-            Страница личного кабинета
-        </div>
+        <Stack
+            flex={1}
+        >
+            <Typography
+                variant='h2'
+                align='center'
+                sx={{ padding: '20px' }}
+                fontWeight='fontWeightBold'
+            >
+                Личный кабинет
+            </Typography>
+        
+            <Typography variant='h3' align='center'>
+                Василий Пупкин
+            </Typography>
+            <Stack
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'space-around',
+                }}
+            >
+                <PersonlAvatar />
+
+                <Mail />
+
+            </Stack>
+
+        </Stack>
     )
 }

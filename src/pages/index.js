73d6858c6ec73476ad774f131e32b52d028b7dc3
code-injection <krunch12345@gmail.com>
2022-10-14 17:08:21 +0300

    pages/index: added RegistrationPage

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import { NewProjectPage } from './NewProjectPage/components/NewProjectPage.jsx'
 import { PersonalArea } from './PersonalArea/components/PersonalArea.jsx'
 import { ProjectsPage } from './ProjectsPage/components/ProjectsPage.jsx'
 import { NotFoundPage } from './NotFoundPage/components/NotFoundPage.jsx'
+import { RegistrationPage } from './RegistrationPage/components/RegistrationPage.jsx'
 
 export {
     MainPage,
@@ -15,5 +16,6 @@ export {
     PersonalArea,
     NewProjectPage,
     ProjectsPage,
-    NotFoundPage
+    NotFoundPage,
+    RegistrationPage
 }

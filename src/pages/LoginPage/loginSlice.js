022f8cfb17c1fd44fd3e121a005e559cfab9218d
code-injection <krunch12345@gmail.com>
2022-10-14 17:05:33 +0300

    loginSlice: created

---
+++ src/pages/LoginPage/loginSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { api } from '../../store/api'
+
+const initialState = {
+    email: null,
+    token: null,
+}
+
+export const loginSlice = createSlice({
+    name: 'login',
+
+    initialState,
+
+    reducers: {
+        loggedOut: (state) => {
+            state.rememberMe = false
+            state.email = null
+            state.token = null
+        },
+
+        doRememberMe: (state) => {
+            state.rememberMe = true
+        },
+    },
+
+    // extraReducers(builder) {
+    //     builder.addMatcher(
+    //         api.endpoints.signin.matchFulfilled,
+    //         (state, { payload }) => {
+    //             state.email = payload.email
+    //             state.token = payload.token
+    //         },
+    //     )
+    // },
+})
+
+export const { loggedOut, doRememberMe } = loginSlice.actions
+export const loginSelector = (state) => state.login

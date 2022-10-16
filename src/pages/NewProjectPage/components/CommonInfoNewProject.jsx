67ee2e672ca1c3436436753b44113bf2c46fb924
Kirill Shkola <kishkola.work@gmail.com>
2022-10-16 08:21:26 +0300

    Update CommonInfoNewProject.jsx
    Колонка общей информации

--- src/pages/NewProjectPage/components/CommonInfoNewProject.jsx
+++ src/pages/NewProjectPage/components/CommonInfoNewProject.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
-
+import { Stack, TextField } from '@mui/material'
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 export const CommonInfoNewProject = () => {
+    const [deadlineValue, setDeadlineValue] = React.useState(null)
+
     return (
-        <div>
-            Общая информация
-        </div>
+        <Stack
+            width='100%'
+            spacing={3}
+        >
+            <TextField
+                variant='outlined'
+                fullWidth
+                label='Название проекта*'
+            />
+
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                    label='Дедлайн*'
+                    value={deadlineValue}
+                    onChange={(newValue) => {
+                        setDeadlineValue(newValue)
+                    }}
+                    mask=''
+                    renderInput={(params) =>
+                        <TextField {...params} />
+                    }
+                />
+            </LocalizationProvider>
+        </Stack>
     )
 }

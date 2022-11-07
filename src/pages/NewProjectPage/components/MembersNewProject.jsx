d609bec0a669aa0a6cdaadc383dc4b77e69f4e23
Kirill Shkola <kishkola.work@gmail.com>
2022-11-07 23:18:19 +0300

    Функционал добавления участников в проект

--- src/pages/NewProjectPage/components/MembersNewProject.jsx
+++ src/pages/NewProjectPage/components/MembersNewProject.jsx
@@ -1,25 +1,78 @@
 import React from 'react'
 import {
-    Avatar,
     Button,
     FormControl,
     IconButton,
     InputLabel,
     List,
     ListItem,
-    ListItemAvatar,
     ListItemText,
     MenuItem,
     Select,
-    Stack
+    Stack,
+    Typography
 } from '@mui/material'
-import { Add, EditOutlined } from '@mui/icons-material'
-import { fakeMembers } from '../constants/fakeMembers'
-import { fakeRoles } from '../constants/fakeRoles'
+import { Add, Clear } from '@mui/icons-material'
+import { useGetUsersQuery } from '../../../store/api'
+import { Modal, ProgressOverlay } from '../../../common/index'
+import { useDispatch } from 'react-redux'
+import { addUserId } from '../newProjectPageSlice'
 
 export const MembersNewProject = () => {
-    const onSubmit = (e) => {
-        e.preventDefault()
+    const [projectMemberId, setProjectMemberId ] = React.useState('')
+    const [projectMembers, setProjectMember] = React.useState([])
+    const [showDeleteMemberModal, setShowDeleteMemberModal] = React.useState(false)
+
+    const { data: users, isFetching } = useGetUsersQuery()
+
+    const generateUserList = (allUsers, checkedUsers) => {
+        const isAddedUser = (user) =>
+            checkedUsers.find(item => item.id === user._id) && true || false
+
+        const userList = () => {
+            return allUsers?.reduce(
+                (acc, item) =>
+                    isAddedUser(item) ? [...acc] : [...acc, item],
+                [])
+        }
+
+        return userList()?.map(
+            (item) => (
+                <MenuItem
+                    value={item._id}
+                    key={item._id}
+                >
+                    {`${item.lastName} ${item.firstName} ${item.patronymicName}`}
+                </MenuItem>
+            )
+        )
+    }
+
+    const usersOptions = React.useMemo(
+        () => generateUserList(users, projectMembers), [users, projectMembers]
+    )
+
+    const dispatch = useDispatch()
+
+    const addMember = () => {
+        const id = projectMemberId
+        const user = users?.find(item => item._id === id)
+        const name = `${user.lastName} ${user.firstName} ${user.patronymicName}`
+
+        setProjectMember(oldValues => [...oldValues, { id, name }])
+        setProjectMemberId(() => '')
+
+        dispatch (
+            addUserId(id)
+        )
+    }
+
+    const deleteMember = (id) => {
+        setShowDeleteMemberModal(false)
+        const index = projectMembers.findIndex(item => item.id === id)
+        if (index !== -1) {
+            projectMembers.splice(index, 1)
+        }
     }
 
     return (
@@ -28,13 +81,10 @@ export const MembersNewProject = () => {
             spacing={3}
         >
             <Stack
-                component='form'
-                onSubmit={onSubmit}
-                method='POST'
                 spacing={3}
                 alignItems='center'
             >
-                <FormControl fullWidth required>
+                <FormControl fullWidth>
                     <InputLabel id='age-select-label'>
                         Участник
                     </InputLabel>
@@ -43,20 +93,24 @@ export const MembersNewProject = () => {
                         labelId='age-select-label'
                         id='age-select'
                         label='Участник'
-                        value='name'
-                    >
-                        {fakeMembers.map((item) =>
-                            <MenuItem
-                                value={item.name}
-                                key={item.id}
+                        value={projectMemberId}
+                        onChange={(e) =>
+                            setProjectMemberId(e.target.value)
+                        }
+                        endAdornment={
+                            <IconButton
+                                sx={{ visibility: projectMemberId ? 'visible' : 'hidden' }}
+                                onClick={() => setProjectMemberId('')}
                             >
-                                {item.name}
-                            </MenuItem>
-                        )}
+                                <Clear />
+                            </IconButton>
+                        }
+                    >
+                        {usersOptions}
                     </Select>
                 </FormControl>
 
-                <FormControl fullWidth required>
+                {/* <FormControl fullWidth required>
                     <InputLabel id='role-select-label'>
                         Роль
                     </InputLabel>
@@ -76,39 +130,64 @@ export const MembersNewProject = () => {
                             </MenuItem>
                         )}
                     </Select>
-                </FormControl>
+                </FormControl> */}
 
                 <Button
-                    type='submit'
                     variant='outlined'
                     startIcon={<Add />}
                     sx={{ borderRadius: '20px' }}
+                    onClick={addMember}
+                    disabled={!projectMemberId}
                 >
                     добавить участника
                 </Button>
             </Stack>
 
             <List>
-                {fakeMembers.map((item) =>
+                {projectMembers.map((item) =>
                     <ListItem
                         key={item.id}
                         secondaryAction={
                             <IconButton>
-                                <EditOutlined />
+                                {/* <EditOutlined /> */}
+                                <Modal
+                                    button='icon'
+                                    isOpen={showDeleteMemberModal}
+                                    isOutlintedVariant
+                                    showCheck
+                                    allowSubmit
+                                    error
+                                    onSubmit={() => deleteMember(item.id)}
+                                    onOpen={() => setShowDeleteMemberModal(true)}
+                                    onClose={() => setShowDeleteMemberModal(false)}
+                                    icon={<Clear />}
+                                    label='Удалить участника'
+                                    title='Удаление участника'
+                                    del
+                                >
+                                    Вы уверены, что хотите удалить участника:
+                                    <Typography>
+                                        {item.name}
+                                    </Typography>
+                                </Modal>
                             </IconButton>
                         }
                     >
-                        <ListItemAvatar>
+                        {/* <ListItemAvatar>
                             <Avatar src={item.avatar} />
-                        </ListItemAvatar>
+                        </ListItemAvatar> */}
 
                         <ListItemText
                             primary={item.name}
-                            secondary={item.role}
+                            // secondary={item.role}
                         />
                     </ListItem>
                 )}
             </List>
+
+            {isFetching && (
+                <ProgressOverlay showProgressOverlay={isFetching} />
+            )}
         </Stack>
     )
 }

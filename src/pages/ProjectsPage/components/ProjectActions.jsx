3f368ecadeb6e04635147296fd6ab5480bf10c47
kirill3518 <kirill3518@mail.ru>
2022-11-07 21:46:30 +0500

    MenuItem component replaced with Modal

--- src/pages/ProjectsPage/components/ProjectActions.jsx
+++ src/pages/ProjectsPage/components/ProjectActions.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import {
     Menu,
-    MenuItem,
     Stack,
-    Typography,
-    IconButton,
+    IconButton
 } from '@mui/material'
 import {
     LibraryAdd,
@@ -14,8 +12,14 @@ import {
     Message,
     LibraryAddCheck
 } from '@mui/icons-material'
+import { Modal } from '../../../common/index.js'
 
 export const ProjectActions = () => {
+    const [openModal_1, setOpenModal_1] = React.useState(false)
+    const [openModal_2, setOpenModal_2] = React.useState(false)
+    const [openModal_3, setOpenModal_3] = React.useState(false)
+    const [openModal_4, setOpenModal_4] = React.useState(false)
+    const [openModal_5, setOpenModal_5] = React.useState(false)
     const [anchorEl, setAnchorEl] = React.useState(null)
     const open = Boolean(anchorEl)
     const handleClick = (event) => {
@@ -49,75 +53,60 @@ export const ProjectActions = () => {
                 open={open}
                 onClose={handleClose}
             >
-                <MenuItem>
-                    <Stack
-                        direction='row'
-                        spacing={2}
-                        flex={1}
-                        justifyContent='space-between'
-                    >
-                        <Typography>
-                            Изменить статус
-                        </Typography>
-                        <LibraryAdd />
-                    </Stack>
-                </MenuItem>
+                <Stack
+                    direction='column'
+                    alignItems='flex-start'
+                >
+                    <Modal
+                        button='menuItem'
+                        isOpen={openModal_1}
+                        onOpen={() => setOpenModal_1(true)}
+                        onClose={() => setOpenModal_1(false)}
+                        icon={<LibraryAdd />}
+                        label='Изменить статус'
+                        title='Изменить статус'
+                    />
 
-                <MenuItem>
-                    <Stack
-                        direction='row'
-                        spacing={2}
-                        flex={1}
-                        justifyContent='space-between'
-                    >
-                        <Typography>
-                            Изменить решение
-                        </Typography>
-                        <LibraryAddCheck />
-                    </Stack>
-                </MenuItem>
+                    <Modal
+                        button='menuItem'
+                        isOpen={openModal_2}
+                        onOpen={() => setOpenModal_2(true)}
+                        onClose={() => setOpenModal_2(false)}
+                        icon={<LibraryAddCheck />}
+                        label='Изменить решение'
+                        title='Изменить решение'
+                    />
 
-                <MenuItem>
-                    <Stack
-                        direction='row'
-                        spacing={2}
-                        flex={1}
-                        justifyContent='space-between'
-                    >
-                        <Typography>
-                            Открыть комментарии
-                        </Typography>
-                        <Message />
-                    </Stack>
-                </MenuItem>
+                    <Modal
+                        button='menuItem'
+                        isOpen={openModal_3}
+                        onOpen={() => setOpenModal_3(true)}
+                        onClose={() => setOpenModal_3(false)}
+                        icon={<Message />}
+                        label='Открыть комментарии'
+                        title='Открыть комментарии'
+                    />
 
-                <MenuItem>
-                    <Stack
-                        direction='row'
-                        spacing={2}
-                        flex={1}
-                        justifyContent='space-between'
-                    >
-                        <Typography>
-                            Документы
-                        </Typography>
-                        <Article />
-                    </Stack>
-                </MenuItem>
+                    <Modal
+                        button='menuItem'
+                        isOpen={openModal_4}
+                        onOpen={() => setOpenModal_4(true)}
+                        onClose={() => setOpenModal_4(false)}
+                        icon={<Article />}
+                        label='Документы'
+                        title='Документы'
+                    />
 
-                <MenuItem>
-                    <Stack
-                        direction='row'
-                        spacing={2}
-                        flex={1}
-                        justifyContent='space-between'
-                    >
-                        <Typography>
-                            Удалить проект
-                        </Typography>
-                        <DeleteOutline />
-                    </Stack>
-                </MenuItem>
+                    <Modal
+                        button='menuItem'
+                        isOpen={openModal_5}
+                        onOpen={() => setOpenModal_5(true)}
+                        onClose={() => setOpenModal_5(false)}
+                        icon={<DeleteOutline />}
+                        label='Удалить проект'
+                        title='Удалить проект'
+                    />
+                </Stack>
             </Menu>
         </>
     )

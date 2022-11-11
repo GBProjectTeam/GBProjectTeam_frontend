5d9250639bd7f138b4dfe1e6f0f488128fbc33d2
code-injection <krunch12345@gmail.com>
2022-11-11 15:15:57 +0300

    common/index: added DeleteModal

--- src/common/index.js
+++ src/common/index.js
@@ -2,10 +2,12 @@ import { Header } from './Header/components/Header.jsx'
 import { Modal } from './Modal/components/Modal.jsx'
 import { PrivateRoute } from './PrivateRoute/components/PrivateRoute.jsx'
 import { ProgressOverlay } from './ProgressOverlay/components/ProgressOverlay.jsx'
+import { DeleteModal } from './DeleteModal/components/DeleteModal.jsx'
 
 export {
     Header,
     Modal,
     PrivateRoute,
     ProgressOverlay,
+    DeleteModal,
 }

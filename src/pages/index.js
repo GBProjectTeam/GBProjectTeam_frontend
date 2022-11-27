0bc8d351c4b9c6ee6c8ceb426b5637240aa2410e
code-injection <krunch12345@gmail.com>
2022-11-27 17:05:07 +0300

    pages/info: PersonalPage > ProfilePage

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { LoginPage } from './LoginPage/components/LoginPage.jsx'
 import { MainPage } from './MainPage/components/MainPage.jsx'
 import { ArchivePage } from './ArchivePage/components/ArchivePage.jsx'
 import { NewProjectPage } from './NewProjectPage/components/NewProjectPage.jsx'
-import { PersonalPage } from './PersonalPage/components/PersonalPage.jsx'
+import { ProfilePage } from './ProfilePage/components/ProfilePage.jsx'
 import { ProjectsPage } from './ProjectsPage/components/ProjectsPage.jsx'
 import { NotFoundPage } from './NotFoundPage/components/NotFoundPage.jsx'
 import { RegistrationPage } from './RegistrationPage/components/RegistrationPage.jsx'
@@ -14,7 +14,7 @@ export {
     ApprovalPage,
     LoginPage,
     ArchivePage,
-    PersonalPage,
+    ProfilePage,
     NewProjectPage,
     ProjectsPage,
     NotFoundPage,
